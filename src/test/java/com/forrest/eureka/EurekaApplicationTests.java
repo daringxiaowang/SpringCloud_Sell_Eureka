9738a23d4a9896b1a9d7734344d88488bda77021
forrest <1776768993@qq.com>
2019-10-09 22:08:39 +0800

    secord commit

---
+++ src/test/java/com/forrest/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forrest.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
